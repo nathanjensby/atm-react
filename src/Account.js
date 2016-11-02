cf3dc9153f6b95c04340b55d7994bca98e8c6a29
Nathan Jensby <njensby@gmail.com>
2016-11-01 19:52:14 -0500

    added event listeners and handleDeposit function

--- src/Account.js
+++ src/Account.js
@@ -7,14 +7,27 @@ export default class Account extends Component {
       balance:0
     }
   }
+
+  _handleDeposit() {
+    let amount = this.refs.amt.value;
+    let newAmount = parseInt(amount, 10) + parseInt(this.state.balance, 10);
+    this.setState({
+      balance: newAmount
+    })
+  }
+
+  _handleWithdrawal() {
+    
+  }
+
   render() {
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
         <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <input type="text" placeholder="enter an amount" ref="amt"/>
+        <input type="button" value="Deposit" ref="deposit" onClick={this._handleDeposit.bind(this)}/>
+        <input type="button" value="Withdraw" ref="withdraw" onClick={this._handleWithdrawal.bind(this)}/>
       </div>
     )
   }
